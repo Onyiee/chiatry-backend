5d3c2aac69ae4baca0692ed15a06c8ef0c228538
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-09-15 14:41:02 +0100

    project setup

---
+++ web/src/main/java/com/chiatry/ChiatryApplication.java
@@ -0,0 +1,13 @@
+package com.chiatry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiatryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiatryApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/chiatry/ChiatryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiatry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiatryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
